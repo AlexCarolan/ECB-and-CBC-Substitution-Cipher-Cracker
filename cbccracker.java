8fd12ed9c92a34f9ba6ef6829ea1c88a614087a5
Carolan <carolana@egb076000050.lancs.local>
2019-02-03 17:18:41 +0000

    Added CBC cracker

--- cbccracker.java
+++ cbccracker.java
@@ -5,14 +5,17 @@ import java.lang.StringBuilder;
 import java.io.PrintWriter;
 import java.io.IOException;
 
-public class ecbcracker {
+public class cbccracker {
 	
-	public void start (String cipherTextName, String plainTextName, String SecondCipherTextName, String outputFileName) {	
+	public void start (String cipherTextName, String plainTextName, String SecondCipherTextName, String outputFileName, int IV_One, int IV_Two) {	
 		//Read all file data for both texts
 		
 		String plainText = "";
 		String cipherText = "";
 		String secondCipherText = "";
+		
+		// IF 26 or larger - 26
+		// to find letter subrtact previous and map
 		
 		try{
 			plainText = new String (Files.readAllBytes(Paths.get(plainTextName)));
@@ -29,25 +32,39 @@ public class ecbcracker {
 		HashMap<Character, Character> letterMap = new HashMap<>();
 		HashMap<Character, Character> reverseLetterMap = new HashMap<>();
 		
+		//get array of normal alphabet positions
+		char[] alphabet = "abcdefghijklmnopqrstuvwxyz".toCharArray();
+		
+		char prevChar = (char)(IV_One + 97);
+		
+		
+		
 		//Itterate through the plain and cipher text
 		for (int i=0; i < plainText.length(); i++){
 			char pChar = plainText.charAt(i);        
 			char cChar = cipherText.charAt(i);
 			
+			//Compute addition mod(26) of plain char and IV/prev char
+			int modChar = pChar + (prevChar - 97);
+			if (modChar > 122){
+				modChar = modChar - 26;
+			}
+			
+			prevChar = cChar;
+			
 			//Map each letter of the cipher text to the plain text
-			letterMap.put(pChar, cChar);
-			reverseLetterMap.put(cChar, pChar);
+			letterMap.put((char)modChar, cChar);
+			reverseLetterMap.put(cChar, (char)modChar);
 		}
 		
-		//get array of normal alphabet positions
-		char[] alphabet = "abcdefghijklmnopqrstuvwxyz".toCharArray();
 		
 		//Print out key letter pairings
-		System.out.println("Mode: ECB, Key:");
+		System.out.println("Mode: CBC, Key:");
 		for (int i=0; i<alphabet.length; i++){	
 			System.out.println(alphabet[i] + "->" + letterMap.get(alphabet[i]));
 		}
 		
+		/*
 		//Create a string builder to assemble the plain text
 		StringBuilder plainTextBuilder = new StringBuilder(); 
 		
@@ -67,12 +84,11 @@ public class ecbcracker {
 			System.out.println("Unable to output file");
 			System.out.println(e);
 		}
-			
+			*/
 	}
 	
 			
 		
 
 }
-
 
